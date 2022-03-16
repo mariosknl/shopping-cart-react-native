259523ef6b27d08ce59175577bbb60292be15b77
mariosknl <mariosknl@outlook.com>
2022-03-16 12:59:36 +0200

    installing dependencies and creating folder structure

--- App.js
+++ App.js
@@ -2,13 +2,7 @@ import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Universal React with Expo</Text>
     </View>
   );
