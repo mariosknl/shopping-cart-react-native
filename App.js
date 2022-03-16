865b2ab6a0e521864fcbb702223ed723b10a88e1
mariosknl <mariosknl@outlook.com>
2022-03-16 16:54:01 +0200

    using custom fonts with AppLoading component from expo

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 
 import productsReducer from "./store/reducers/products";
 import ShopNavigator from "./navigation/ShopNavigator";
@@ -10,7 +13,25 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
   return (
     <Provider store={store}>
       <ShopNavigator />
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -49,8 +49,10 @@ const styles = StyleSheet.create({
     color: "#888",
     textAlign: "center",
     marginVertical: 20,
+    fontFamily: "open-sans-bold",
   },
   description: {
+    fontFamily: "open-sans",
     fontSize: 14,
     textAlign: "center",
     marginHorizontal: 20,
