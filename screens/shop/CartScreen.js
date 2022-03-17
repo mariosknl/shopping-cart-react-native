4c015df308503fef2384f66c215d0e1edf11eaa8
mariosknl <mariosknl@outlook.com>
2022-03-17 10:01:14 +0200

    adding logic to delete items from cart

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { startDetecting } from "react-native/Libraries/Utilities/PixelRatio";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
+import * as cartActions from "../../store/actions/cart";
 
 const CartScreen = (props) => {
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
@@ -19,8 +20,11 @@ const CartScreen = (props) => {
         sum: state.cart.items[key].sum,
       });
     }
-    return transformedCartItems;
+    return transformedCartItems.sort((a, b) =>
+      a.productId > b.productId ? 1 : -1
+    );
   });
+  const dispatch = useDispatch();
 
   return (
     <View style={styles.screen}>
@@ -43,7 +47,9 @@ const CartScreen = (props) => {
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
             amount={itemData.item.sum}
-            onRemove={() => {}}
+            onRemove={() => {
+              dispatch(cartActions.removeFromCart(itemData.item.productId));
+            }}
           />
         )}
       />
