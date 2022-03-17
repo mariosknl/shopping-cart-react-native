066275d53fdb6b083c33058b5e20c4049920a3e6
mariosknl <mariosknl@outlook.com>
2022-03-17 09:33:25 +0200

    implementing header button & cart functionality

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 import productsReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
@@ -14,7 +13,7 @@ const rootReducer = combineReducers({
   cart: cartReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
---
+++ screens/shop/CartScreen.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import { startDetecting } from "react-native/Libraries/Utilities/PixelRatio";
+import { useSelector } from "react-redux";
+
+import Colors from "../../constants/Colors";
+
+const CartScreen = (props) => {
+  const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const cartItems = useSelector((state) => {
+    const transformedCartItems = [];
+    for (const key in state.cart.items) {
+      transformedCartItems.push({
+        productId: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        sum: state.cart.items[key].sum,
+      });
+    }
+    return transformedCartItems;
+  });
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.summary}>
+        <Text style={styles.summaryText}>
+          Total:{" "}
+          <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
+        </Text>
+        <Button
+          title="Order Now"
+          color={Colors.accent}
+          disabled={cartItems.length === 0}
+        />
+      </View>
+      <View>
+        <Text>CART ITEMS</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    margin: 20,
+  },
+  summary: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    padding: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+  },
+  summaryText: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+  },
+  amount: {
+    color: Colors.primary,
+  },
+});
+
+export default CartScreen;
