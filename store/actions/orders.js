a484cfeb86e37a47c72095ff34e945faa5eb4284
mariosknl <mariosknl@outlook.com>
2022-03-18 14:34:56 +0200

    storing orders to the firebase database

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,37 @@
 export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount },
+  return async (dispatch) => {
+    const date = new Date();
+    const response = await fetch(
+      "https://rn-shopping-a6eac-default-rtdb.europe-west1.firebasedatabase.app/orders/u1.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date,
+      },
+    });
   };
 };
