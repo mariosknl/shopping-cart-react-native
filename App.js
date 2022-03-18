f6632b1e7587ffe2741e4002aa36b1f5e3791117
mariosknl <mariosknl@outlook.com>
2022-03-18 12:47:11 +0200

    installing redux thunk and set applyMiddleware to the App component

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
+import ReduxThunk from "redux-thunk";
 
 import productsReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
@@ -15,7 +16,7 @@ const rootReducer = combineReducers({
   orders: orderReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const fetchFonts = () => {
   return Font.loadAsync({
